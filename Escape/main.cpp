562bece132811786adafcb2a0445bbe85584aa1f
schaefer <rene.schaefer@rwth-aachen.de>
2016-11-09 20:29:25 +0100

    NIX

--- Escape/main.cpp
+++ Escape/main.cpp
@@ -6,7 +6,7 @@
 int main(int args, char* argv[])
 {
 
-	std::cout << "Hallo Welt, jetzt bin ich auch da :D" << std::endl;
+	std::cout << "Hallo Welt" << std::endl;
 	std::cout << "Added a new line of code" << std::endl;
 
 	system("PAUSE");
