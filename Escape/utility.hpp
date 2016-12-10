52dbb0667103e3e0b0fb7741c7175425e939859e
Maged Mortaga <mortaga@outlook.com>
2016-12-10 15:39:43 +0100

    Habe SDL komplett entfernt und GLFW installiert

--- Escape/utility.hpp
+++ Escape/utility.hpp
@@ -4,29 +4,8 @@
 // Inclusion of the standard IO library
 #include <iostream>
 
-#include <SDL.h>
 #include <glew.h>
 
-
-// This function is used to log events/information to the console
-void logMsg(const char* pMessage)
-{
-	std::clog << pMessage << std::endl;
-}
-
-void logErr(const char* pMessage, unsigned int pStatus)
-{
-	switch (pStatus)
-	{
-		case 0:
-			std::cout << pMessage << ": " << SDL_GetError() << std::endl;
-			break;
-		case 1:
-			std::cout << pMessage << ": " << glGetError() << std::endl;
-		default:
-			std::cout << "LogErr: " << "You choose a invalid status number!" << std::endl;
-	}
-}
 
 
 
