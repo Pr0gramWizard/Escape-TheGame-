0308b27322a51352bbfa68ce848b3c7a53039386
schaefer <rene.schaefer@rwth-aachen.de>
2016-12-25 19:34:05 +0100

    Add model attribute to entity

--- Escape/entity.hpp
+++ Escape/entity.hpp
@@ -2,11 +2,12 @@
 
 #include <vec3.hpp>
 #include <glew.h>
+#include "model.hpp"
 
 class Entity
 {
 public:
-	Entity(glm::vec3 pPosition, GLfloat pXRotation, GLfloat pYRotation, GLfloat pZRotation, GLfloat pScale);
+	Entity(glm::vec3 pPosition, GLfloat pXRotation, GLfloat pYRotation, GLfloat pZRotation, GLfloat pScale, Model* pModel);
 	~Entity();
 
 	glm::vec3 getPosition() const;
@@ -14,6 +15,7 @@ public:
 	GLfloat getYRotation() const;
 	GLfloat getZRotation() const;
 	GLfloat getScale() const;
+	Model* getModel();
 
 	void increasePosition(GLfloat x, GLfloat y, GLfloat z);
 	void increaseRotation(GLfloat x, GLfloat y, GLfloat z);
@@ -31,5 +33,6 @@ private:
 	GLfloat mZRotation;
 	//  scale (1 = normal size)
 	GLfloat mScale;
+	Model* mModel;
 };
 
