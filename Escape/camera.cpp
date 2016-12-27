56a4617e4224b521d1451b90f3e795571a4d5a23
Maged <Maged@LAPTOP-MAGED>
2016-12-27 13:11:58 +0100

    Disabled Fullscreen

--- Escape/camera.cpp
+++ Escape/camera.cpp
@@ -63,8 +63,8 @@ void Camera::ProcessMouseMovement(GLfloat pXOffset, GLfloat pYOffset, GLfloat de
 {
 	GLboolean constrainPitch = true;
 
-	pXOffset *= (this->getMouseSensitivity() * 100) * deltaTime;
-	pYOffset *= (this->getMouseSensitivity() * 100) * deltaTime;
+	pXOffset *= (this->getMouseSensitivity() * 200) * deltaTime;
+	pYOffset *= (this->getMouseSensitivity() * 200) * deltaTime;
 
 	this->setYaw(this->getYaw() - pXOffset);
 	this->setPitch(this->getPitch() - pYOffset);
