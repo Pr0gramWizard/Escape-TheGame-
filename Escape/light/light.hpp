0cf25a6356565b735dce1e6db56c3e64f801eee9
schaefer <rene.schaefer@rwth-aachen.de>
2016-12-31 15:57:24 +0100

    Add light class

---
+++ Escape/light/light.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <vec3.hpp>
+
+class Light {
+
+public:
+	Light(glm::vec3 pPosition, glm::vec3 pColor);
+	Light(glm::vec3 pPosition, glm::vec3 pColor, glm::vec3 pAttenuation);
+	~Light();
+
+	// Setters
+	void setPosition(glm::vec3 pPosition);
+	void setColor(glm::vec3 pColor);
+	void setAttenuation(glm::vec3 pAttenuation);
+
+	// Getters
+	glm::vec3 getPosition() const;
+	glm::vec3 getColor() const;
+	glm::vec3 getAttenuation() const;
+
+	// Increases position by an offset
+	void incPosition(float pX, float pY, float pZ);
+
+	// Inverts the light's color
+	void invertColor();
+
+private:
+	glm::vec3 mPosition;
+	glm::vec3 mColor;
+	glm::vec3 mAttenuation;
+};
