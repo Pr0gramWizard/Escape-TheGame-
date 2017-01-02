831644a65621ce861698309ffaef079569b76ac3
schaefer <rene.schaefer@rwth-aachen.de>
2017-01-02 17:57:13 +0100

    Add some empty methods to lake

--- Escape/lake/lake.cpp
+++ Escape/lake/lake.cpp
@@ -1,1 +1,71 @@
 #include "lake.hpp"
+
+const int Lake::LAKE_SIZE = 20;
+
+Lake::Lake(int pWorldX, int pWorldY, int pAmplitude, int pVertices, const char * pName, Loader * pLoader)
+{
+}
+
+Lake::~Lake()
+{
+}
+
+int Lake::getWorldX() const
+{
+	return 0;
+}
+
+int Lake::getWorldZ() const
+{
+	return 0;
+}
+
+glm::vec2 Lake::getWorldPos() const
+{
+	return glm::vec2();
+}
+
+const char * Lake::getName()
+{
+	return nullptr;
+}
+
+void Lake::setAmplitude(int pAmplitude)
+{
+}
+
+void Lake::setName(const char * pName)
+{
+}
+
+glm::mat4 Lake::getModelMatrix()
+{
+	return glm::mat4();
+}
+
+Model * Lake::getModel()
+{
+	return nullptr;
+}
+
+void Lake::setModel(Model * pModel)
+{
+}
+
+void Lake::updateHeights()
+{
+}
+
+Model Lake::generateLake(Loader * loader)
+{
+	return Model();
+}
+
+void Lake::initLake(Loader * loader)
+{
+}
+
+GLfloat Lake::getHeight(int pVertexX, int pVertexZ)
+{
+	return GLfloat();
+}
--- Escape/lake/lake.hpp
+++ Escape/lake/lake.hpp
@@ -1,1 +1,55 @@
 #pragma once
+
+#include <glew.h>
+#include <iostream>
+#include <vec2.hpp>
+#include <vector>
+// model class
+#include "../model.hpp"
+// loader class
+#include "../loader.hpp"
+// math class
+#include "../math.hpp"
+
+class Lake
+{
+public:
+	Lake(int pWorldX, int pWorldY, int pAmplitude, int pVertices, const char* pName, Loader* pLoader);
+	~Lake();
+
+	// Getter functions
+	int getWorldX() const;
+	int getWorldZ() const;
+	glm::vec2 getWorldPos() const;
+	const char* getName();
+
+	// Setter functions
+	void setAmplitude(int pAmplitude);
+	void setName(const char* pName);
+
+	glm::mat4 getModelMatrix();
+	Model* getModel();
+	void setModel(Model* pModel);
+
+	void updateHeights();
+public:
+	static const int LAKE_SIZE;
+
+private:
+	int mWorldX;
+	int mWorldZ;
+	int mAmplitude;
+	// Getters & Setters needed? Maybe static
+	int mVertices;
+	float mGridSize;
+	const char* mName;
+	Model mModel;
+	std::vector<float> mHeights;
+	std::vector<float> mVelocity;
+
+private:
+	Model generateLake(Loader* loader);
+	void initLake(Loader* loader);
+	GLfloat getHeight(int pVertexX, int pVertexZ);
+};
+
