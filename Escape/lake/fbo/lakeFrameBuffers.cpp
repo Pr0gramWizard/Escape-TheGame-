b277c74620c5be0e61db38efb1828dd12e504226
schaefer <rene.schaefer@rwth-aachen.de>
2017-01-03 18:51:01 +0100

    Add empty methods for lake frame buffers

--- Escape/lake/fbo/lakeFrameBuffers.cpp
+++ Escape/lake/fbo/lakeFrameBuffers.cpp
@@ -1,6 +1,10 @@
 #include "lakeFrameBuffers.hpp"
 
+const int LakeFrameBuffers::REFLECTION_WIDTH = 320;
+const int LakeFrameBuffers::REFLECTION_HEIGHT = 180;
 
+const int LakeFrameBuffers::REFRACTION_WIDTH = 1280;
+const int LakeFrameBuffers::REFRACTION_HEIGHT = 720;
 
 LakeFrameBuffers::LakeFrameBuffers()
 {
@@ -9,4 +13,62 @@ LakeFrameBuffers::LakeFrameBuffers()
 
 LakeFrameBuffers::~LakeFrameBuffers()
 {
+}
+
+void LakeFrameBuffers::bindReflectionFrameBuffer()
+{
+}
+
+void LakeFrameBuffers::bindRefractionFrameBuffer()
+{
+}
+
+void LakeFrameBuffers::unbindCurrentFrameBuffer()
+{
+}
+
+GLuint LakeFrameBuffers::getReflactionTexture()
+{
+	return GLuint();
+}
+
+GLuint LakeFrameBuffers::getRefractionTexture()
+{
+	return GLuint();
+}
+
+GLuint LakeFrameBuffers::getRefractionDepthTexture()
+{
+	return GLuint();
+}
+
+void LakeFrameBuffers::cleanUp()
+{
+}
+
+void LakeFrameBuffers::initReflectionBuffer()
+{
+}
+
+void LakeFrameBuffers::initRefractionBuffer()
+{
+}
+
+void LakeFrameBuffers::bindFrameBuffer(GLuint pBuffer, GLuint pWidth, GLuint pHeight)
+{
+}
+
+GLuint LakeFrameBuffers::createFrameBuffer()
+{
+	return GLuint();
+}
+
+GLuint LakeFrameBuffers::createTextureAttachment(GLuint pWidth, GLuint pHeight)
+{
+	return GLuint();
+}
+
+GLuint LakeFrameBuffers::createDepthTextureAttachment(GLuint pWidth, GLuint pHeight)
+{
+	return GLuint();
 }
--- Escape/lake/fbo/lakeFrameBuffers.hpp
+++ Escape/lake/fbo/lakeFrameBuffers.hpp
@@ -1,8 +1,53 @@
 #pragma once
+
+#include <glew.h>
+
 class LakeFrameBuffers
 {
 public:
 	LakeFrameBuffers();
 	~LakeFrameBuffers();
-};
 
+	// binding buffers
+	void bindReflectionFrameBuffer();
+	void bindRefractionFrameBuffer();
+	void unbindCurrentFrameBuffer();
+
+	// texture getters
+	GLuint getReflactionTexture();
+	GLuint getRefractionTexture();
+	GLuint getRefractionDepthTexture();
+
+	// clean up
+	void cleanUp();
+
+public:
+	static const int REFLECTION_WIDTH;
+	static const int REFLECTION_HEIGHT;
+
+	static const int REFRACTION_WIDTH;
+	static const int REFRACTION_HEIGHT;
+
+private:
+	// init functions
+	void initReflectionBuffer();
+	void initRefractionBuffer();
+
+	// binding a framebuffer
+	void bindFrameBuffer(GLuint pBuffer, GLuint pWidth, GLuint pHeight);
+
+	// create functions
+	GLuint createFrameBuffer();
+	GLuint createTextureAttachment(GLuint pWidth, GLuint pHeight);
+	GLuint createDepthTextureAttachment(GLuint pWidth, GLuint pHeight);
+
+
+private:
+	GLuint mReflectionFrameBuffer;
+	GLuint mReflectionTexture;
+	GLuint mReflectionDepthBuffer;
+
+	GLuint mRefractionFrameBuffer;
+	GLuint mRefractionTexture;
+	GLuint mRefractionDepthTexture;
+};
