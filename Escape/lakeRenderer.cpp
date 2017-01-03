5d9636475e87dc5517fd09cae849abbc5237a48e
schaefer <rene.schaefer@rwth-aachen.de>
2017-01-03 02:02:09 +0100

    Add code to lake renderer

--- Escape/lakeRenderer.cpp
+++ Escape/lakeRenderer.cpp
@@ -1,12 +1,102 @@
 #include "lakeRenderer.hpp"
 
+// constructor
+LakeRenderer::LakeRenderer(LakeShader * pShader, glm::mat4 pProjectionMatrix)
+{
+	mShader = pShader;
+	mShader->use();
+	mShader->loadProjectionMatrix(pProjectionMatrix);
+	mShader->unuse();
+}
 
-
-lakeRenderer::lakeRenderer()
+// destructor
+LakeRenderer::~LakeRenderer()
 {
 }
 
-
-lakeRenderer::~lakeRenderer()
+// renders a single Lake
+void LakeRenderer::render(Lake &pLake)
 {
+	prepareLake(&pLake);
+	loadModelMatrix(&pLake);
+	glDrawElements(GL_TRIANGLES, pLake.getModel()->getVerticesCount(), GL_UNSIGNED_INT, 0);
+	unbindLake();
 }
+
+
+// binds the vertex array and the needed attributes for the Lake
+void LakeRenderer::prepareLake(Lake pLake)
+{
+	Model* model = pLake.getModel();
+	glBindVertexArray(model->getVaoId());
+	glEnableVertexAttribArray(0);
+	glEnableVertexAttribArray(1);
+	glEnableVertexAttribArray(2);
+	// Texturepart here
+
+	// end texturepart
+}
+
+// prepares the renderer for rendering
+void LakeRenderer::prepare()
+{
+	glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+}
+
+// binds the vertex array and the needed attributes for the Lake
+void LakeRenderer::prepareLake(Lake* pLake)
+{
+	Model* model = pLake->getModel();
+	glBindVertexArray(model->getVaoId());
+	glEnableVertexAttribArray(0);
+	glEnableVertexAttribArray(1);
+	glEnableVertexAttribArray(2);
+	// Texturepart here
+
+	// end texturepart
+}
+
+// disables the vertex array and the used attibutes
+void LakeRenderer::unbindLake()
+{
+	glDisableVertexAttribArray(0);
+	glDisableVertexAttribArray(1);
+	glDisableVertexAttribArray(2);
+	glBindVertexArray(0);
+}
+
+// loads the model matrix for a Lake
+void LakeRenderer::loadModelMatrix(Lake pLake)
+{
+	mShader->loadModelMatrix(pLake.getModelMatrix());
+}
+
+// loads the model matrix for a Lake
+void LakeRenderer::loadModelMatrix(Lake* pLake)
+{
+	mShader->loadModelMatrix(pLake->getModelMatrix());
+}
+
+void LakeRenderer::loadViewMatrix(glm::mat4 pViewMatrix)
+{
+	mShader->loadViewMatrix(pViewMatrix);
+}
+
+void LakeRenderer::loadLights(vector<Light*> pLights)
+{
+	mShader->loadLights(pLights);
+}
+
+void LakeRenderer::startShader()
+{
+	mShader->use();
+}
+
+void LakeRenderer::stopShader()
+{
+	mShader->unuse();
+}
+
+
+
--- Escape/lakeRenderer.hpp
+++ Escape/lakeRenderer.hpp
@@ -1,8 +1,52 @@
 #pragma once
-class lakeRenderer
+
+#include "lakeshader.hpp"
+// matrix class
+#include <mat4x4.hpp>
+// List
+#include <list>
+// lake class
+#include "lake\lake.hpp"
+// model class
+#include "model.hpp"
+// maths
+#include "math.hpp"
+// light class
+#include "light\light.hpp"
+
+using namespace std;
+
+class LakeRenderer
 {
 public:
-	lakeRenderer();
-	~lakeRenderer();
+	// constructor
+	LakeRenderer(LakeShader* pShader, glm::mat4 pProjectionMatrix);
+	// destructor
+	~LakeRenderer();
+
+	// render methods
+	void render(Lake &pTerrain);
+
+	// methods that get called before the acutal rendering
+	void prepare();
+	void prepareLake(Lake pLake);
+	void prepareLake(Lake* pLake);
+
+	// unbinding all terrains
+	void unbindLake();
+
+	// load model matrix for a terrain into the shader
+	void loadModelMatrix(Lake pLake);
+	void loadModelMatrix(Lake* pLake);
+	void loadViewMatrix(glm::mat4 pViewMatrix);
+	void loadLights(vector<Light*> pLights);
+
+	// use or unuse the shader
+	void startShader();
+	void stopShader();
+
+
+private:
+	LakeShader* mShader;
 };
 
