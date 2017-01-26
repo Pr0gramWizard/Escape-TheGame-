28ce0303a04f92cffe5a007f8db04cc88b84ff87
schaefer <rene.schaefer@rwth-aachen.de>
2017-01-26 19:00:01 +0100

    Add blur shader

--- Escape/blur/blurBuffer.hpp
+++ Escape/blur/blurBuffer.hpp
@@ -1,8 +1,35 @@
 #pragma once
-class BlurrFrameBuffer
+
+// GLEW Library
+#include <glew.h>
+// Input/Output Stream
+#include <iostream>
+// blur shader
+#include "./shader/blurShader.hpp"
+
+class BlurFrameBuffers
 {
 public:
-	BlurrFrameBuffer();
-	~BlurrFrameBuffer();
+	BlurFrameBuffers(BlurShader * pShader, GLuint pWidth, GLuint pHeight);
+	~BlurFrameBuffers();
+
+	void setHorizontal(bool pHorizontal);
+	bool getHorizontal() const;
+	void prepare();
+	void startShader();
+	void stopShader();
+
+private:
+	void init(GLuint pWidth, GLuint pHeight);
+
+private:
+	// fbos for horizontal and vertical blur
+	GLuint mFBO[2];
+	// color buffers for the fbos
+	GLuint mColorbuffers[2];
+	// used for switching between horizontal and vertical blur
+	bool mHorizontal;
+	// blur shader
+	BlurShader* mShader;
 };
 
