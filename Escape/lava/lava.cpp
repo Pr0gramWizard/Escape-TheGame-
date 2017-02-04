1d79e4a9b7dbd9f75999bf473cb80fdd8332c67b
schaefer <rene.schaefer@rwth-aachen.de>
2017-02-04 20:14:23 +0100

    changes to lava files

--- Escape/lava/lava.cpp
+++ Escape/lava/lava.cpp
@@ -7,11 +7,9 @@ Lava::Lava(int pWorldX, int pWorldY, int pWorldZ, int pAmplitude, int pVertices,
 	mWorldX = pWorldX;
 	mWorldY = pWorldY;
 	mWorldZ = pWorldZ;
-	this->setAmplitude(pAmplitude);
 	mVertices = pVertices;
 	this->setName(pName);
 
-	this->initLava(pLoader);
 	this->setModel(&this->generateLava(pLoader));
 	mPosVbo = (GLuint) pLoader->getLastVbos().y;
 	mNormalVbo = (GLuint) pLoader->getLastVbos().z;
@@ -46,11 +44,6 @@ const char * Lava::getName()
 	return mName;
 }
 
-void Lava::setAmplitude(int pAmplitude)
-{
-	mAmplitude = pAmplitude;
-}
-
 void Lava::setName(const char * pName)
 {
 	mName = pName;
@@ -71,67 +64,24 @@ void Lava::setModel(Model * pModel)
 	mModel = *pModel;
 }
 
-GLfloat Lava::getVertexHeight(int pVertexX, int pVertexZ)
-{
-	if (pVertexX < 0)
-	{
-		pVertexX = 0;
-	}
-	else if (pVertexX >= mVertices)
-	{
-		pVertexX = mVertices - 1;
-	}
-	
-	if (pVertexZ < 0) 
-	{
-		pVertexZ = 0;
-	}
-	else if (pVertexZ >= mVertices)
-	{
-		pVertexZ = mVertices - 1;
-	}
-	
-	return mHeights[pVertexZ * mVertices + pVertexX];
-}
-
-void Lava::initLava(Loader * loader)
-{
-	mVelocity = vector<float>(mVertices * mVertices, 0.0f);
-	mHeights = vector<float>(mVertices * mVertices, (float) this->getWorldY());
-	mVaryingPositions = vector<float>(mVertices * mVertices * 3, 0.0f);
-	mVaryingNormals = vector<float>(mVertices * mVertices * 3, 0.0f);
-
-	int half = (int)(mVertices / 2.0f);
-
-	mVelocity[half * mVertices + half] = -5;
-	mVelocity[half * mVertices + half - 1] = -2.5f;
-	mVelocity[half * mVertices + half + 1] = -2.5f;
-	mVelocity[(half + 1) * mVertices + half] = -2.5f;
-	mVelocity[(half - 1) * mVertices + half] = -2.5f;
-
-	// generates random heights between -mAmplitude and +mAmplitude
-	/*for (int i = 0; i < mHeights.size(); ++i)
-	{
-		mHeights[i] = ((float)rand() / (float)(RAND_MAX)) * (2 * mAmplitude) - mAmplitude;
-	}*/
-}
-
 Model Lava::generateLava(Loader * loader)
 {
 	// set grid size
 	mGridSize = (float)Lava::LAVA_SIZE / (mVertices - 1);
 	int count = mVertices * mVertices;
+	std::vector<float> vertices(count * 3);
+	std::vector<float> normals(count * 3);
 	std::vector<float> textureCoords(count * 2);
 	std::vector<int> indices(6 * (mVertices - 1)*(mVertices - 1));
 	int vertexPointer = 0;
 	for (int z = 0;z<mVertices;z++) {
 		for (int x = 0;x<mVertices;x++) {
-			mVaryingPositions[vertexPointer * 3] = (float)x / ((float)mVertices - 1) * Lava::LAVA_SIZE;
-			mVaryingPositions[vertexPointer * 3 + 1] = this->getVertexHeight(x, z);
-			mVaryingPositions[vertexPointer * 3 + 2] = (float)z / ((float)mVertices - 1) * Lava::LAVA_SIZE;
-			mVaryingNormals[vertexPointer * 3] = 0;
-			mVaryingNormals[vertexPointer * 3 + 1] = 1;
-			mVaryingNormals[vertexPointer * 3 + 2] = 0;
+			vertices[vertexPointer * 3] = (float)x / ((float)mVertices - 1) * Lava::LAVA_SIZE;
+			vertices[vertexPointer * 3 + 1] = this->mWorldY;
+			vertices[vertexPointer * 3 + 2] = (float)z / ((float)mVertices - 1) * Lava::LAVA_SIZE;
+			normals[vertexPointer * 3] = 0;
+			normals[vertexPointer * 3 + 1] = 1;
+			normals[vertexPointer * 3 + 2] = 0;
 			textureCoords[vertexPointer * 2] = (float)x / ((float)mVertices - 1);
 			textureCoords[vertexPointer * 2 + 1] = (float)z / ((float)mVertices - 1);
 			vertexPointer++;
@@ -153,66 +103,5 @@ Model Lava::generateLava(Loader * loader)
 		}
 	}
 
-	return loader->loadDataToVao(mVaryingPositions, textureCoords, mVaryingNormals, indices);
-}
-
-void Lava::updateVelocities()
-{
-	for (int z = 1;z < mVertices - 1;z++) {
-		for (int x = 1;x < mVertices - 1;x++) {
-			mVelocity[z * mVertices + x] += ((this->getVertexHeight(x - 1, z) + this->getVertexHeight(x + 1, z) + this->getVertexHeight(x, z - 1) + this->getVertexHeight(x, z + 1)) / 4 - this->getVertexHeight(x, z));
-			mVelocity[z * mVertices + x] *= 0.99f;
-		}
-	}
-}
-
-void Lava::updateNormals()
-{
-	int vertexPointer = 0;
-	// update the normals
-	float hL, hR, hD, hU;
-	for (int z = 1;z < mVertices - 1;z++) {
-		for (int x = 1;x < mVertices - 1;x++) {
-
-			hL = mHeights[z * mVertices + (x - 1)]; // this->getVertexHeight(x - 1, z);
-			hR = mHeights[z * mVertices + (x + 1)]; //this->getVertexHeight(x + 1, z);
-			hD = mHeights[(z - 1) * mVertices + x]; //this->getVertexHeight(x, z - 1);
-			hU = mHeights[(z + 1) * mVertices + x]; //this->getVertexHeight(x, z + 1);
-
-			// deduce terrain normal
-			glm::vec3 normal = glm::normalize(glm::vec3(hL - hR, 2.0f, hD - hU));
-			mVaryingNormals[vertexPointer * 3] = normal.x;
-			mVaryingNormals[vertexPointer * 3 + 1] = normal.y;
-			mVaryingNormals[vertexPointer * 3 + 2] = normal.z;
-			vertexPointer++;
-		}
-	}
-}
-
-void Lava::updatePositionVBO()
-{
-	// update position vbo
-	glBindBuffer(GL_ARRAY_BUFFER, mPosVbo);
-	glBufferSubData(GL_ARRAY_BUFFER, 0, mVaryingPositions.size() * sizeof(float), &mVaryingPositions[0]);
-	// update normal vbo
-	glBindBuffer(GL_ARRAY_BUFFER, mNormalVbo);
-	glBufferSubData(GL_ARRAY_BUFFER, 0, mVaryingNormals.size() * sizeof(float), &mVaryingNormals[0]);
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-}
-
-void Lava::updateHeights(GLfloat pDeltaTime)
-{
-	this->updateVelocities();
-	int vertexPointer = 0;
-	// update the heights
-	for (int z = 1;z < mVertices -1;z++) {
-		for (int x = 1;x < mVertices - 1;x++) {
-			mHeights[z * mVertices + x] += mVelocity[z * mVertices + x] * pDeltaTime;
-			// update vertices
-			mVaryingPositions[vertexPointer * 3 + 1] = mHeights[z * mVertices + x];
-			vertexPointer++;
-		}
-	}
-	this->updateNormals();
-	this->updatePositionVBO();
+	return loader->loadDataToVao(vertices, textureCoords, normals, indices);
 }
